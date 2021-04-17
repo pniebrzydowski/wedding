f13453bb3e97fc2167703470ce29c466d3dff424
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2021-04-17 17:02:50 +0200

    return undefined if date is empty for local date util

--- components/admin/utils.ts
+++ components/admin/utils.ts
@@ -2,7 +2,10 @@ import dayjs from 'dayjs';
 
 const tzOffset = dayjs().utcOffset() / 60;
 
-export const getLocalDate = (date: string): string => {
+export const getLocalDate = (date?: string): string | undefined => {
+  if (!date) {
+    return;
+  }
   return dayjs(date)
     .add(tzOffset, 'hour')
     .format('YYYY-MM-DD HH:mm');
